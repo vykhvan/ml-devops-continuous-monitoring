dd46b58a3c80356a97ddac84e43f2a75609c13f2
Vyacheslav Khvan <vykhvan@gmail.com>
2021-09-09 04:54:02 +0000

    Update fullprocess pipeline

--- fullprocess.py
+++ fullprocess.py
@@ -19,46 +19,54 @@ output_model_path = os.path.join(config['output_model_path'])
 
 ##################Check and read new data
 #first, read ingestedfiles.txt
-print("Check new data")
-ingestedfiles = pd.read_csv(prod_deployment_path+"/ingestedfiles.txt", header=None)
-olddata = set(ingestedfiles.iloc[:,2].values)
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
-newdata = set(os.listdir(input_folder_path))
-if len(newdata.difference(olddata)) != 0:
-    print("Run re-ingestion")
-    ingestion.merge_multiple_dataframe(input_folder_path, output_folder_path)
+def run_ingestion():
+    ingestedfiles = pd.read_csv(prod_deployment_path+"/ingestedfiles.txt", header=None)
+    olddata = set(ingestedfiles.iloc[:,2].values)
+    newdata = set(os.listdir(input_folder_path))
+    if len(newdata.difference(olddata)) != 0:
+        print("Run re-ingestion")
+        ingestion.merge_multiple_dataframe(input_folder_path, output_folder_path)
+    else:
+        quit()
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-if len(newdata.difference(olddata)) == 0:
-    print("No new data - end process")
-    quit()
+def run_training():
+    with open(prod_deployment_path+"/latestscore.txt", "r") as file:
+        latestscore = float(file.read())
+    newestscore = scoring.score_model(output_folder_path+"/finaldata.csv", 
+                                      prod_deployment_path)
+    if newestscore < latestscore:
+        print("Re-training")
+        training.train_model()
+    else:
+        print("No model drift")
+        quit()
 
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-print("Check model drift")
-with open(prod_deployment_path+"/latestscore.txt", "r") as file:
-    latestscore = float(file.read())
-newestscore = scoring.score_model(output_folder_path+"/finaldata.csv", prod_deployment_path)
-if newestscore <= latestscore:
-    print("Re-training")
-    training.train_model()
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
-if newestscore > latestscore:
-    print("No model drift")
-    quit()
+def run_scoring():
+    print("Run scoring")
+    os.system("python scoring.py")
 
+def run_deployment():
+    print("Run deployment")
+    deployment.store_model_into_pickle("trainedmodel.pkl")
 
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-print("Re-deployment")
-os.system("python scoring.py")
-deployment.store_model_into_pickle("trainedmodel.pkl")
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
-os.system("python diagnostics.py")
-os.system("python reporting.py")
-os.system("python apicalls.py")
+def run_diagnostics():
+    print("Run diagnostics")
+    os.system("python diagnostics.py")
 
+def run_reporting():
+    print("Run reporting")
+    os.system("python reporting.py")
 
+def run_apicalls():
+    print("Run calling API")
+    os.system("python apicalls.py")
+
+if __name__ == "__main__":
+    run_ingestion()
+    run_training()
+    run_scoring()
+    run_deployment()
+    run_diagnostics()
+    run_reporting()
+    run_apicalls()
+    
